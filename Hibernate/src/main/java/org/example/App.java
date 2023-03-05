7cbbb19754af7f40ba5bba5f3a0018ea7b91ba68
ShivamRoy@gmail.com <126485361+Shivamroy-ai@users.noreply.github.com>
2023-03-05 11:50:56 +0530

    Update App.java
    i

--- Hibernate/src/main/java/org/example/App.java
+++ Hibernate/src/main/java/org/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Change Fast Change World!" );
+        System.out.println( "Hello Change By Shivam World!" );
     }
 }
