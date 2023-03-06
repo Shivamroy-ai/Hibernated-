f2baec295fc4e7d0ebf7a745cb9e9d41dea66bb0
Shivam <Shivam@rawcubes.com>
2023-03-06 07:57:44 +0530

    Make Configration File and Create Session

--- Hibernate/src/main/java/org/example/App.java
+++ Hibernate/src/main/java/org/example/App.java
@@ -1,13 +1,23 @@
 package org.example;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.io.File;
+
 /**
  * Hello world!
- *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello hjkhjk World!" );
+        System.out.println( "Hello World!" );
+
+        Configuration configuration=new Configuration();
+        configuration.configure(new File("Hibernate/src/main/java/hibernate.cfg.xml"));
+
+        SessionFactory sessionFactory=configuration.buildSessionFactory();
+        System.out.println("Session Factory = "+sessionFactory);
     }
 }
